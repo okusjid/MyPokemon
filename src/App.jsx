f135cf884647cfa7f37df98a535083f371a030e3
okusjid <l200927@lhr.nu.edu.pk>
2024-08-15 17:30:51 +0500

    Added cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
 import Card from "./components/Card";
+import { useState } from "react";
 import { useGetAllPokemonQuery } from "./store/Pokemon/pokemonApiSlice";
 
 export default function App() {
   const { data, error, isLoading } = useGetAllPokemonQuery();
+  const[search, setSearch] = useState("");
+
+  const handleSearch = (e) => {
+    console.log(e.target.value);
+    setSearch(e.target.value);
+  };
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Error: {error.message}</div>;
 
   return (
-    <div className=" bg-gradient-to-r from-cyan-500 to-blue-500 h-screen">
+    <div className=" bg-gradient-to-r from-cyan-500 to-blue-500 m-auto">
       <h1 className="text-center font-medium text-3xl  text-blue-300 hover:text-blue-700 hover:font-bold">
         POKEMON
       </h1>
-
-      <div className=" p-4 flex justify-evenly gap-4 flex-wrap">
+      <input type="text" value={search} onChange={() => handleSearch(search)} className="border-2 border-blue-500 rounded-md m-4 p-2"/>
         {data?.results.map((pokemon) => (
           <Card key={pokemon.name} pokemon={pokemon} />
         ))}
       </div>
-    </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,7 @@
+import propType from "prop-type";
 const Card = ({pokemon}) => {
     return (
-        <div className=" border border-spacing-2 border-teal-700 m-5 p-4">
+        <div className=" border border-spacing-2 border-teal-700 m-5 p-4 mb-5">
             <div className="card-content">
                 <h3>Name: {pokemon.name}</h3>
                 {/* <p>Card Description</p> */}
@@ -9,5 +10,13 @@ const Card = ({pokemon}) => {
     );
 
 }
+
+Card.propTypes = {
+    pokemon: propType.shape({
+        name: propType.string.isRequired,
+    }).isRequired,
+};
+
+
 
 export default Card;
